39a3a5feabffde9ca345138876103ce2e34b07f4
anbuksv <anbuksv@gmail.com>
2021-05-27 18:11:03 +0530

    test commit

--- app/test.py
+++ app/test.py
@@ -1,4 +1,4 @@
-aws_secret = 'ABCsDEF+c2L7yXeGvUyrPsYsDnWRRC1AYEXAMPzE'
+aws_secret = 'ABCsDEF+c2L7yXexvUyrPsYsDnWRRC1AYEXAMPzE'
 
 def print_user(first_name,last_name):
     print("Hi %s %s" % (first_name, last_name))
