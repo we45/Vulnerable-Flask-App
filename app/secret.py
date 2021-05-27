fb727d8973e318e0dc7d497ef9cf0845aedcdeb2
anbuksv <anbuksv@gmail.com>
2021-05-27 16:47:40 +0530

    gitleaks test

---
+++ app/secret.py
@@ -0,0 +1,7 @@
+aws_secret_access_key = 'PStuwajFLB634KSxrFCaL0/5lTBZBPmeDdYhTt5E' #it will not detected by default engine
+aws_secret = 'ABCsDEF+c2L7yXeGvUyrPgYsDnWRRC1AYEXAMPLE'
+
+def print_user(first_name,last_name):
+    print("Hi %s %s" % (first_name, last_name))
+
+print_user("Anbarasan", "K")
