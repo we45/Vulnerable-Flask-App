7900ee1567c6bd8e6e5ca874769e4a92741c0269
anbuksv <anbuksv@gmail.com>
2021-05-27 17:08:26 +0530

    some modification

--- app/secret.py
+++ app/secret.py
@@ -1,4 +1,3 @@
-aws_secret_access_key = 'PStuwajFLB634KSxrFCaL0/5lTBZBPmeDdYhTt5E' #it will not detected by default engine
 aws_secret = 'ABCsDEF+c2L7yXeGvUyrPgYsDnWRRC1AYEXAMPLE'
 
 def print_user(first_name,last_name):
